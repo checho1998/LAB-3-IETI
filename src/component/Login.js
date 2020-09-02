3afbe9987ecc7a79aa0c8ad71f6742b4925d61d2
checho1998 <c.hecho.28@hotmail.com>
2020-09-02 17:49:05 -0500

    avance

---
+++ src/component/Login.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import LockIcon from '@material-ui/icons/LockOutlined';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import './Login.css';
+import  NavBar from '../NavBar';
+
+export class Login extends React.Component{
+
+
+    loginSubmit(){
+        const email=document.getElementById("email").value;
+        const password=document.getElementById("password").value
+        console.log(email + " " + password)
+        if( email!=="" &&  password!==""){
+            localStorage.setItem("isLoggedin",true);
+            localStorage.setItem("mailLogged",email);
+            localStorage.setItem("passwordLogged",password);
+        }
+    }
+
+
+    render(){
+        const LoginView = () => (
+            <Login/>
+        );
+
+        const About = () => (
+            <div>
+                <NavBar/>
+            </div>
+        );
+        return (
+            <React.Fragment>
+                <CssBaseline />
+                <main className="layout">
+                    <Paper className="paper">
+                        <Avatar className="avatar">
+                            <LockIcon />
+                        </Avatar>
+                        <Typography variant="headline">Sign in</Typography>
+                        <form className="form">
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="email">Email Address</InputLabel>
+                                <Input id="email" name="email" autoComplete="email" autoFocus />
+                            </FormControl>
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="password">Password</InputLabel>
+                                <Input
+                                    name="password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="current-password"
+                                />
+                            </FormControl>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                onClick = {this.loginSubmit}
+                            >
+                                Sign in
+                            </Button>
+                        </form>
+                    </Paper>
+                </main>
+            </React.Fragment>
+        );
+    }
+}
